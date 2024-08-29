c2c9b210d16ad39b69dc3e01ba1ba17e9290f1b1
Deepak Kumar <74102432+itsmedeepak@users.noreply.github.com>
2024-08-29 13:08:06 +0530

    added cors

--- index.js
+++ index.js
@@ -15,7 +15,11 @@ connectDB();
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // Routes
 app.use("/api/auth", authRoutes);
